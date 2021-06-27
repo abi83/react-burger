20d6ef83a39c6413b00d4fc98c467d6475f7e222
abi83 <vladimir.kromm@yandex.ru>
2021-06-27 17:36:36 +0200

    cards aer almost done

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -4,14 +4,36 @@ import Tabs from './tabs';
 import IngredientCard from './ingredient-card/ingredient-card';
 
 export default class BurgerConstructor extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingCategoriesKeys: Array.from(new Set(this.props.ingredients.map(ing=>ing.type))),
+    }
+    this.translatedCategories = this.translatedCategories.bind(this)
+  };
+  translatedCategories = ()=>{
+    const known = {
+        bun: 'Булки',
+        main: 'Начинки',
+        sauce: 'Соусы',
+      }
+    return this.state.ingCategoriesKeys.map(cat=>known[cat])
+  }
+  
   render() {
+    console.log(this.translatedCategories());
     return (
     <section className={'container'} style={{backgroundColor: 'red'}}>
-      <h2 className={style.header}>Соберите бургер</h2>
+      <h2 className={style.h2}>Соберите бургер</h2>
       <Tabs />
-      <h3>Булки</h3>
-      <IngredientCard ingredient={this.props.ingredients[0]} />
-      <IngredientCard ingredient={this.props.ingredients[1]} />
+      <h3 className={style.h3}>Булки</h3>
+      <div className={style.ingredientsContainer}>
+        
+        
+        <IngredientCard key={1} ingredient={this.props.ingredients[0]} />
+        <IngredientCard key={2} ingredient={this.props.ingredients[1]} />
+        <IngredientCard key={3} ingredient={this.props.ingredients[2]} />
+      </div>
     </section>
   )}
 }
--- src/components/burger-ingredients/ingredient-card/ingredient-card.jsx
+++ src/components/burger-ingredients/ingredient-card/ingredient-card.jsx
@@ -1,10 +1,21 @@
 // import HeadButton from '../head-button/head-button';
 import styles from './ingredient-card.module.css'
+import {CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 
 export default function AppHeader ({ingredient}) {
   return (
     <div className={styles.card}>
-      {ingredient.name}
+      <img className={styles.mainImage} src={ingredient.image_large} alt={ingredient.name}/>
+      <div className={styles.price}>
+        <span>
+          {ingredient.price}
+        </span>
+        <CurrencyIcon type="primary" />
+      </div>
+      <p>
+        {ingredient.name}
+      </p>
+      
     </div>
   )
 };
